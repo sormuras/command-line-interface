dc978508c73da8fa5b113c193184b39d9710f463
Jan Bernitt <jaanbernitt@gmail.com>
2022-11-26 12:02:15 +0100

    Var-ification

--- test/test/JarTests.java
+++ test/test/JarTests.java
@@ -2,21 +2,20 @@ package test;
 
 import static java.lang.invoke.MethodHandles.lookup;
 import static test.Assertions.assertEquals;
+import static test.Assertions.assertEqualsOptional;
 
 import java.util.List;
 import java.util.Optional;
 import main.CommandLineInterface;
 import main.CommandLineInterface.Name;
+import test.JarTests.JarOptions.ChangeDirOptions;
+import test.JarTests.JarOptions.ReleaseOptions;
 
 class JarTests implements JTest {
 
   public static void main(String... args) {
     new JarTests().runTests(args);
   }
-
-  record JarCOptions(String dir, String file) {}
-
-  record JarReleaseOptions(String version, @Name("-C") List<JarCOptions> Cs) {}
 
   record JarOptions(
       @Name({"-c", "--create"}) boolean create,
@@ -25,9 +24,9 @@ class JarTests implements JTest {
       @Name({"-u", "--update"}) boolean update,
       @Name({"-x", "--extract"}) boolean extract,
       @Name({"-d", "--describe-module"}) boolean describeModule,
-      @Name({"-C"}) Optional<JarCOptions> changeDir,
+      @Name({"-C"}) Optional<ChangeDirOptions> changeDir,
       @Name({"-f", "--file"}) Optional<String> file,
-      @Name("--release") Optional<JarReleaseOptions> release,
+      @Name("--release") Optional<ReleaseOptions> release,
       @Name({"-v", "--verbose"}) boolean verbose,
       @Name({"-e", "--main-class"}) Optional<String> mainClass,
       @Name({"-m", "--manifest"}) Optional<String> manifest,
@@ -41,7 +40,12 @@ class JarTests implements JTest {
       @Name("--help:compat") boolean helpCompat,
       @Name("--help-extra") boolean helpExtra,
       @Name("--version") boolean version,
-      String... files) {}
+      String... files) {
+
+    record ChangeDirOptions(String dir, String file) {}
+
+    record ReleaseOptions(String version, @Name("-C") List<ChangeDirOptions> Cs) {}
+  }
 
   private static JarOptions parseInput(String line) {
     return CommandLineInterface.parser(lookup(), JarOptions.class).parse(line.split("\\s+"));
@@ -49,59 +53,60 @@ class JarTests implements JTest {
 
   @Test
   void example1() {
-    JarOptions options = parseInput("--create --file classes.jar Foo.class Bar.class");
+    var options = parseInput("--create --file classes.jar Foo.class Bar.class");
     assertEquals(true, options.create());
-    Assertions.assertEqualsOptional("classes.jar", options.file());
+    assertEqualsOptional("classes.jar", options.file());
     assertEquals(List.of("Foo.class", "Bar.class"), List.of(options.files()));
   }
 
   @Test
   void example2() {
-    JarOptions options =
+    var options =
         parseInput(
             "--create --date=\"2021-01-06T14:36:00+02:00\" --file=classes.jar Foo.class Bar.class");
     assertEquals(true, options.create());
-    Assertions.assertEqualsOptional("\"2021-01-06T14:36:00+02:00\"", options.date());
-    Assertions.assertEqualsOptional("classes.jar", options.file());
+    assertEqualsOptional("\"2021-01-06T14:36:00+02:00\"", options.date());
+    assertEqualsOptional("classes.jar", options.file());
     assertEquals(List.of("Foo.class", "Bar.class"), List.of(options.files()));
   }
 
   @Test
   void example3() {
-    JarOptions options = parseInput("--create --file classes.jar --manifest mymanifest -C foo/ .");
+    var options = parseInput("--create --file classes.jar --manifest mymanifest -C foo/ .");
     assertEquals(true, options.create());
-    Assertions.assertEqualsOptional("classes.jar", options.file());
-    Assertions.assertEqualsOptional("mymanifest", options.manifest());
-    Assertions.assertEqualsOptional(new JarCOptions("foo/", "."), options.changeDir());
+    assertEqualsOptional("classes.jar", options.file());
+    assertEqualsOptional("mymanifest", options.manifest());
+    assertEqualsOptional(new ChangeDirOptions("foo/", "."), options.changeDir());
   }
 
   @Test
   void example4() {
-    JarOptions options =
+    var options =
         parseInput(
             "--create --file foo.jar --main-class com.foo.Main --module-version 1.0 -C foo/classes"
                 + " resources");
     assertEquals(true, options.create());
-    Assertions.assertEqualsOptional("foo.jar", options.file());
-    Assertions.assertEqualsOptional("com.foo.Main", options.mainClass());
-    Assertions.assertEqualsOptional("1.0", options.moduleVersion());
-    JarCOptions cOptions = options.changeDir().orElseThrow();
+    assertEqualsOptional("foo.jar", options.file());
+    assertEqualsOptional("com.foo.Main", options.mainClass());
+    assertEqualsOptional("1.0", options.moduleVersion());
+    var cOptions = options.changeDir().orElseThrow();
     assertEquals("foo/classes", cOptions.dir());
     assertEquals("resources", cOptions.file());
   }
 
   @Test
   void example5() {
-    JarOptions options =
+    var options =
         parseInput(
-            "--create --file foo.jar --main-class com.foo.Hello -C classes . --release 10 -C"
-                + " classes-10 .");
+            """
+                        --create --file foo.jar --main-class com.foo.Hello -C classes .
+                        --release 10 -C classes-10 .""");
     assertEquals(true, options.create());
-    Assertions.assertEqualsOptional("foo.jar", options.file());
-    Assertions.assertEqualsOptional("com.foo.Hello", options.mainClass());
-    Assertions.assertEqualsOptional(new JarCOptions("classes", "."), options.changeDir());
-    Assertions.assertEqualsOptional(
-        new JarReleaseOptions("10", List.of(new JarCOptions("classes-10", "."))),
+    assertEqualsOptional("foo.jar", options.file());
+    assertEqualsOptional("com.foo.Hello", options.mainClass());
+    assertEqualsOptional(new ChangeDirOptions("classes", "."), options.changeDir());
+    assertEqualsOptional(
+        new ReleaseOptions("10", List.of(new ChangeDirOptions("classes-10", "."))),
         options.release());
   }
 }
