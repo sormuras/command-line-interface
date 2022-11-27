74b09e68e36b089bb9a7db24f8a44ee8ad87305f
forax <forax@univ-mlv.fr>
2022-11-27 12:58:04 +0100

    make Assertions public so it is possible to write tests in another package than test

--- test/test/Assertions.java
+++ test/test/Assertions.java
@@ -4,43 +4,46 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
 
-final class Assertions {
+public final class Assertions {
+  private Assertions() {
+    throw new AssertionError();
+  }
 
-  static void fail(String message) {
+  public static void fail(String message) {
     throw new AssertionError(message);
   }
 
-  static void assertTrue(boolean actual) {
+  public static void assertTrue(boolean actual) {
     assertEquals(true, actual);
   }
 
-  static void assertTrue(boolean actual, String message) {
+  public static void assertTrue(boolean actual, String message) {
     assertEquals(true, actual, message);
   }
 
-  static void assertFalse(boolean actual) {
+  public static void assertFalse(boolean actual) {
     assertEquals(false, actual);
   }
 
-  static <T> void assertEqualsOptional(T expected, Optional<? extends T> actual) {
+  public static <T> void assertEqualsOptional(T expected, Optional<? extends T> actual) {
     assertEquals(expected, actual.orElseThrow());
   }
 
-  static <T> void assertEquals(T expected, T actual) {
+  public static <T> void assertEquals(T expected, T actual) {
     assertEquals(expected, actual, "");
   }
 
-  static <T> void assertEquals(T expected, T actual, String message) {
+  public static <T> void assertEquals(T expected, T actual, String message) {
     if (Objects.equals(expected, actual)) return;
     fail(expected + " != " + actual + (message.isEmpty() ? "" : ": " + message));
   }
 
-  static <T> void assertArrayEquals(T[] expected, T[] actual) {
+  public static <T> void assertArrayEquals(T[] expected, T[] actual) {
     if (Arrays.equals(expected, actual)) return;
     fail(Arrays.toString(expected) + " != " + Arrays.toString(actual));
   }
 
-  static <E extends RuntimeException> E assertThrows(Class<E> expected, Runnable test) {
+  public static <E extends RuntimeException> E assertThrows(Class<? extends E> expected, Runnable test) {
     try {
       test.run();
       fail("Expected a " + expected.getSimpleName() + " exception but nothing was thrown.");
