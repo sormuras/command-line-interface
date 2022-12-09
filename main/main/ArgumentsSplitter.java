9a58253d0143e0dd71f44cd4aed6af1489d20b0f
forax <forax@univ-mlv.fr>
2022-12-09 10:21:41 +0100

    refactor so a schema can be specified without a record

--- main/main/ArgumentsSplitter.java
+++ main/main/ArgumentsSplitter.java
@@ -1,5 +1,7 @@
 package main;
 
+import main.ArgumentsSplitter.Option.Type;
+
 import static java.lang.Boolean.parseBoolean;
 import static java.util.Objects.requireNonNull;
 import static java.util.function.Predicate.not;
@@ -27,15 +29,17 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.StringJoiner;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public final class ArgumentsSplitter<R extends Record> {
-  public static <R extends Record> ArgumentsSplitter<R> of(Lookup lookup, Class<R> schema) {
+  public static <R extends Record> ArgumentsSplitter<R> of(Lookup lookup, Class<? extends R> schema) {
     return new ArgumentsSplitter<>(lookup, schema);
   }
 
@@ -51,7 +55,7 @@ public final class ArgumentsSplitter<R extends Record> {
     String[] value();
   }
 
-  record Option(Type type, Set<String> names, String help, Class<? extends Record> nestedSchema) {
+  public record Option(Type type, Set<String> names, String help, Schema<?> nestedSchema) {
     public enum Type {
       /** An optional flag, like {@code --verbose}. */
       FLAG(false),
@@ -92,34 +96,6 @@ public final class ArgumentsSplitter<R extends Record> {
       if (names.isEmpty()) throw new IllegalArgumentException("no name defined");
     }
 
-    public static List<Option> scan(Class<? extends Record> schema) {
-      requireNonNull(schema, "schema is null");
-      var recordComponents = schema.getRecordComponents();
-      if (recordComponents == null) {
-        throw new IllegalArgumentException("the schema is not a record");
-      }
-      return Arrays.stream(recordComponents).map(Option::of).toList();
-    }
-
-    public static Option of(RecordComponent component) {
-      requireNonNull(component, "component is null");
-      var nameAnno = component.getAnnotation(Name.class);
-      var helpAnno = component.getAnnotation(Help.class);
-      var names =
-          nameAnno != null
-              ? new LinkedHashSet<>(Arrays.asList(nameAnno.value()))
-              : Set.of(component.getName().replace('_', '-'));
-      var type = Type.valueOf(component.getType());
-      var help = helpAnno != null ? String.join("\n", helpAnno.value()) : "";
-      var nestedSchema =
-          (component.getGenericType() instanceof ParameterizedType paramType
-                  && paramType.getActualTypeArguments()[0] instanceof Class<?> nestedType
-                  && nestedType.isRecord())
-              ? nestedType.asSubclass(Record.class)
-              : null;
-      return new Option(type, names, help, nestedSchema);
-    }
-
     String name() {
       return names.iterator().next();
     }
@@ -137,160 +113,174 @@ public final class ArgumentsSplitter<R extends Record> {
     }
   }
 
-  private final Lookup lookup;
-  private final Class<R> schema;
-  private final List<Option> options;
-  private final ArgumentsProcessor processor;
-  private final boolean nested;
+  public static class Schema<V> {
+    private final List<Option> options;
+    private final Function<? super Object[], ? extends V> finalizer;
 
-  public ArgumentsSplitter(Lookup lookup, Class<R> schema) {
+    public Schema(List<? extends Option> options, Function<? super Object[], ? extends V> finalizer) {
+      requireNonNull(options, "options is null");
+      requireNonNull(finalizer, "finalizer is null");
+      var opts = List.<Option>copyOf(options);
+      if (opts.isEmpty()) throw new IllegalArgumentException("At least one option is expected");
+      checkDuplicates(opts);
+      checkVarargs(opts);
+      this.options = opts;
+      this.finalizer = finalizer;
+    }
+
+    private static void checkDuplicates(List<Option> options) {
+      var optionsByName = new HashMap<String, Option>();
+      for (var option : options) {
+        var names = option.names();
+        for (var name : names) {
+          var otherOption = optionsByName.put(name, option);
+          if (otherOption == option)
+            throw new IllegalArgumentException(
+                "option " + option + " declares duplicated name " + name);
+          if (otherOption != null)
+            throw new IllegalArgumentException(
+                "options " + option + " and " + otherOption + " both declares name " + name);
+        }
+      }
+    }
+
+    private static void checkVarargs(List<Option> options) {
+      var varargs = options.stream().filter(Option::isVarargs).toList();
+      if (varargs.size() > 1) {
+        throw new IllegalArgumentException("Too many varargs types specified: " + varargs);
+      }
+      if (varargs.size() == 1 && !(options.get(options.size() - 1).isVarargs())) {
+        throw new IllegalArgumentException("varargs is not at last index: " + options);
+      }
+    }
+
+    public V split(List<? extends String> args) {
+      Objects.requireNonNull(args, "args is null");
+      return split(new ArrayDeque<>(args), false);
+    }
+
+    private V split(ArrayDeque<String> pendingArguments, boolean nested) {
+      var requiredOptions =
+          options.stream().filter(Option::isRequired).collect(toCollection(ArrayDeque::new));
+      var optionsByName = new HashMap<String, Option>();
+      var workspace = new LinkedHashMap<String, Object>();
+      var flagCount = options.stream().filter(Option::isFlag).count();
+      var flagPattern = flagCount == 0 ? null : Pattern.compile("^-[a-zA-Z]{1," + flagCount + "}$");
+      for (var option : options) {
+        for (var name : option.names()) {
+          optionsByName.put(name, option);
+        }
+        workspace.put(option.name(), option.type().defaultValue());
+      }
+
+      while (true) {
+        if (pendingArguments.isEmpty()) {
+          if (requiredOptions.isEmpty()) return finalizer.apply(workspace.values().toArray());
+          throw new IllegalArgumentException("Required option(s) missing: " + requiredOptions);
+        }
+        // acquire next argument
+        var argument = pendingArguments.removeFirst();
+        int separator = argument.indexOf('=');
+        var noValue = separator == -1;
+        var maybeName = noValue ? argument : argument.substring(0, separator);
+        var maybeValue = noValue ? "" : argument.substring(separator + 1);
+        // try well-known option first
+        if (optionsByName.containsKey(maybeName)) {
+          var option = optionsByName.get(maybeName);
+          var name = option.name();
+          workspace.put(
+              name,
+              switch (option.type()) {
+                case FLAG -> noValue || parseBoolean(maybeValue);
+                case KEY_VALUE -> {
+                  var nestedSchema = option.nestedSchema();
+                  var value =
+                      nestedSchema != null
+                          ? nestedSchema.split(pendingArguments, true)
+                          : noValue ? pendingArguments.pop() : maybeValue;
+                  yield Optional.of(value);
+                }
+                case REPEATABLE -> {
+                  var nestedSchema = option.nestedSchema();
+                  var value =
+                      nestedSchema != null
+                          ? List.of(nestedSchema.split(pendingArguments, true))
+                          : noValue
+                          ? List.of(pendingArguments.pop())
+                          : List.of(maybeValue.split(","));
+                  var elements = (List<?>) workspace.get(name);
+                  yield Stream.concat(elements.stream(), value.stream()).toList();
+                }
+                case VARARGS, REQUIRED -> throw new AssertionError("Unnamed name? " + name);
+              });
+          continue;
+        }
+        // maybe a combination of single letter flags?
+        if (flagPattern != null && flagPattern.matcher(argument).matches()) {
+          var flags = argument.substring(1).chars().mapToObj(c -> "-" + (char) c).toList();
+          if (flags.stream().allMatch(optionsByName::containsKey)) {
+            flags.forEach(flag -> workspace.put(optionsByName.get(flag).name(), true));
+            continue;
+          }
+        }
+        // try required option
+        if (!requiredOptions.isEmpty()) {
+          var requiredOption = requiredOptions.pop();
+          workspace.put(requiredOption.name(), argument);
+          continue;
+        }
+        // restore pending arguments deque
+        pendingArguments.addFirst(argument);
+        if (nested) return finalizer.apply(workspace.values().toArray());
+        // try globbing all pending arguments into a varargs collector
+        var varargsOption = options.get(options.size() - 1);
+        if (varargsOption.isVarargs()) {
+          workspace.put(varargsOption.name(), pendingArguments.toArray(String[]::new));
+          return finalizer.apply(workspace.values().toArray());
+        }
+        throw new IllegalArgumentException("Unhandled arguments: " + pendingArguments);
+      }
+    }
+
+    public String help(int indent) {
+      if (indent < 0) throw new IllegalArgumentException("invalid indent " + indent);
+      var joiner = new StringJoiner("\n");
+      for (var option : options.stream().sorted(Comparator.comparing(Option::name)).toList()) {
+        var text = option.help();
+        if (text.isEmpty()) continue;
+        var suffix =
+            switch (option.type()) {
+              case FLAG -> " (flag)";
+              case KEY_VALUE -> " <value>";
+              case REPEATABLE -> " <value> (repeatable)";
+              case REQUIRED -> " (required)";
+              case VARARGS -> "...";
+            };
+        var names = String.join(", ", option.names());
+        joiner.add(names + suffix);
+        joiner.add(text.indent(indent).stripTrailing());
+      }
+      return joiner.toString();
+    }
+  }
+
+  private final Schema<? extends R> schema;
+
+  private final ArgumentsProcessor processor;
+
+  public ArgumentsSplitter(Lookup lookup, Class<? extends R> schema) {
     this(lookup, schema, ArgumentsProcessor.DEFAULT);
   }
 
-  public ArgumentsSplitter(Lookup lookup, Class<R> schema, ArgumentsProcessor processor) {
-    this(lookup, schema, Option.scan(schema), processor);
+  public ArgumentsSplitter(Lookup lookup, Class<? extends R> schema, ArgumentsProcessor processor) {
+    this(createSchemaFromRecord(lookup, schema), processor);
   }
 
-  private ArgumentsSplitter(
-      Lookup lookup, Class<R> schema, List<Option> options, ArgumentsProcessor processor) {
-    this(lookup, schema, options, processor, false);
-  }
-
-  private ArgumentsSplitter(
-      Lookup lookup,
-      Class<R> schema,
-      List<Option> options,
-      ArgumentsProcessor processor,
-      boolean nested) {
-    requireNonNull(lookup, "lookup is null");
+  private ArgumentsSplitter(Schema<? extends R> schema, ArgumentsProcessor processor) {
     requireNonNull(schema, "schema is null");
-    requireNonNull(options, "options is null");
     requireNonNull(processor, "processor is null");
-    var opts = List.copyOf(options);
-    if (opts.isEmpty()) throw new IllegalArgumentException("At least one option is expected");
-    checkDuplicates(opts);
-    checkVarargs(opts);
-    this.lookup = lookup;
     this.schema = schema;
-    this.options = opts;
     this.processor = processor;
-    this.nested = nested;
-  }
-
-  private static void checkDuplicates(List<Option> options) {
-    var optionsByName = new HashMap<String, Option>();
-    for (var option : options) {
-      var names = option.names();
-      for (var name : names) {
-        var otherOption = optionsByName.put(name, option);
-        if (otherOption == option)
-          throw new IllegalArgumentException(
-              "option " + option + " declares duplicated name " + name);
-        if (otherOption != null)
-          throw new IllegalArgumentException(
-              "options " + option + " and " + otherOption + " both declares name " + name);
-      }
-    }
-  }
-
-  private static void checkVarargs(List<Option> options) {
-    var varargs = options.stream().filter(Option::isVarargs).toList();
-    if (varargs.size() > 1) {
-      throw new IllegalArgumentException("Too many varargs types specified: " + varargs);
-    }
-    if (varargs.size() == 1 && !(options.get(options.size() - 1).isVarargs())) {
-      throw new IllegalArgumentException("varargs is not at last index: " + options);
-    }
-  }
-
-  private Object[] split(ArrayDeque<String> pendingArguments) {
-    var requiredOptions =
-        options.stream().filter(Option::isRequired).collect(toCollection(ArrayDeque::new));
-    var optionsByName = new HashMap<String, Option>();
-    var workspace = new LinkedHashMap<String, Object>();
-    var flagCount = options.stream().filter(Option::isFlag).count();
-    var flagPattern = flagCount == 0 ? null : Pattern.compile("^-[a-zA-Z]{1," + flagCount + "}$");
-    for (var option : options) {
-      for (var name : option.names()) {
-        optionsByName.put(name, option);
-      }
-      workspace.put(option.name(), option.type().defaultValue());
-    }
-
-    while (true) {
-      if (pendingArguments.isEmpty()) {
-        if (requiredOptions.isEmpty()) return workspace.values().toArray();
-        throw new IllegalArgumentException("Required option(s) missing: " + requiredOptions);
-      }
-      // acquire next argument
-      var argument = pendingArguments.removeFirst();
-      int separator = argument.indexOf('=');
-      var noValue = separator == -1;
-      var maybeName = noValue ? argument : argument.substring(0, separator);
-      var maybeValue = noValue ? "" : argument.substring(separator + 1);
-      // try well-known option first
-      if (optionsByName.containsKey(maybeName)) {
-        var option = optionsByName.get(maybeName);
-        var name = option.name();
-        workspace.put(
-            name,
-            switch (option.type()) {
-              case FLAG -> noValue || parseBoolean(maybeValue);
-              case KEY_VALUE -> {
-                var value =
-                    option.nestedSchema() != null
-                        ? splitNested(pendingArguments, option)
-                        : noValue ? pendingArguments.pop() : maybeValue;
-                yield Optional.of(value);
-              }
-              case REPEATABLE -> {
-                var value =
-                    option.nestedSchema() != null
-                        ? List.of(splitNested(pendingArguments, option))
-                        : noValue
-                            ? List.of(pendingArguments.pop())
-                            : List.of(maybeValue.split(","));
-                var elements = (List<?>) workspace.get(name);
-                yield Stream.concat(elements.stream(), value.stream()).toList();
-              }
-              case VARARGS, REQUIRED -> throw new AssertionError("Unnamed name? " + name);
-            });
-        continue;
-      }
-      // maybe a combination of single letter flags?
-      if (flagPattern != null && flagPattern.matcher(argument).matches()) {
-        var flags = argument.substring(1).chars().mapToObj(c -> "-" + (char) c).toList();
-        if (flags.stream().allMatch(optionsByName::containsKey)) {
-          flags.forEach(flag -> workspace.put(optionsByName.get(flag).name(), true));
-          continue;
-        }
-      }
-      // try required option
-      if (!requiredOptions.isEmpty()) {
-        var requiredOption = requiredOptions.pop();
-        workspace.put(requiredOption.name(), argument);
-        continue;
-      }
-      // restore pending arguments deque
-      pendingArguments.addFirst(argument);
-      if (nested) return workspace.values().toArray();
-      // try globbing all pending arguments into a varargs collector
-      var varargsOption = options.get(options.size() - 1);
-      if (varargsOption.isVarargs()) {
-        workspace.put(varargsOption.name(), pendingArguments.toArray(String[]::new));
-        return workspace.values().toArray();
-      }
-      throw new IllegalArgumentException("Unhandled arguments: " + pendingArguments);
-    }
-  }
-
-  private Object splitNested(ArrayDeque<String> pendingArguments, Option option) {
-    var nestedSchema = option.nestedSchema;
-    var nestedOptions = Option.scan(nestedSchema);
-    var nestedSplitter =
-        new ArgumentsSplitter<>(lookup, nestedSchema, nestedOptions, processor, true);
-    var constructor = constructor(lookup, nestedSchema);
-    return createRecord(constructor, nestedSplitter.split(pendingArguments));
   }
 
   public R split(String... args) {
@@ -300,10 +290,42 @@ public final class ArgumentsSplitter<R extends Record> {
 
   public R split(Stream<String> args) {
     requireNonNull(args, "args is null");
-    var constructor = constructor(lookup, schema).asFixedArity();
     var values = processor.process(args).collect(toCollection(ArrayDeque::new));
-    var arguments = split(values);
-    return schema.cast(createRecord(constructor, arguments));
+    return schema.split(values, false);
+  }
+
+  private static List<Option> createOptions(Lookup lookup, Class<? extends Record> schema) {
+    var recordComponents = schema.getRecordComponents();
+    if (recordComponents == null) {
+      throw new IllegalArgumentException("the schema is not a record");
+    }
+    return Arrays.stream(recordComponents).map(c -> createOption(lookup, c)).toList();
+  }
+
+  private static Option createOption(Lookup lookup, RecordComponent component) {
+    var nameAnno = component.getAnnotation(Name.class);
+    var helpAnno = component.getAnnotation(Help.class);
+    var names =
+        nameAnno != null
+            ? new LinkedHashSet<>(Arrays.asList(nameAnno.value()))
+            : Set.of(component.getName().replace('_', '-'));
+    var type = Type.valueOf(component.getType());
+    var help = helpAnno != null ? String.join("\n", helpAnno.value()) : "";
+    var nestedRecordType =
+        (component.getGenericType() instanceof ParameterizedType paramType
+            && paramType.getActualTypeArguments()[0] instanceof Class<?> nestedType
+            && nestedType.isRecord())
+            ? nestedType.asSubclass(Record.class)
+            : null;
+    var nestedSchema = nestedRecordType == null ? null : createSchemaFromRecord(lookup, nestedRecordType);
+    return new Option(type, names, help, nestedSchema);
+  }
+
+  private static <R extends Record> Schema<R> createSchemaFromRecord(Lookup lookup, Class<R> schema) {
+    requireNonNull(lookup, "lookup is null");
+    requireNonNull(schema, "schema is null");
+    var constructor = constructor(lookup, schema).asFixedArity();
+    return new Schema<>(createOptions(lookup, schema), args -> schema.cast(createRecord(constructor, args)));
   }
 
   private static MethodHandle constructor(Lookup lookup, Class<?> schema) {
@@ -331,24 +353,7 @@ public final class ArgumentsSplitter<R extends Record> {
   }
 
   public String help(int indent) {
-    if (indent < 0) throw new IllegalArgumentException("invalid indent " + indent);
-    var joiner = new StringJoiner("\n");
-    for (var option : options.stream().sorted(Comparator.comparing(Option::name)).toList()) {
-      var text = option.help();
-      if (text.isEmpty()) continue;
-      var suffix =
-          switch (option.type()) {
-            case FLAG -> " (flag)";
-            case KEY_VALUE -> " <value>";
-            case REPEATABLE -> " <value> (repeatable)";
-            case REQUIRED -> " (required)";
-            case VARARGS -> "...";
-          };
-      var names = String.join(", ", option.names());
-      joiner.add(names + suffix);
-      joiner.add(text.indent(indent).stripTrailing());
-    }
-    return joiner.toString();
+    return schema.help(indent);
   }
 
   @FunctionalInterface
