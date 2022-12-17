353a1fd17428c5e4f8c8505991c48994840bcbf6
forax <forax@univ-mlv.fr>
2022-12-17 08:35:35 +0100

    Simplify to use only ArgumentBag, either the arguments are associated to a user provided object (enum, string, etc) or the option first name is used

--- test/test/EnumTests.java
+++ test/test/EnumTests.java
@@ -6,16 +6,14 @@ import main.Splitter;
 import test.api.JTest;
 import test.api.JTest.Test;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Spliterator;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static java.util.Collections.unmodifiableMap;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toMap;
 import static java.util.stream.IntStream.range;
@@ -32,164 +30,19 @@ class EnumTests {
     JTest.runTests(new EnumTests(), args);
   }
 
+
   // ---
 
-  public record Result(Option.Type type, Object rawValue) {
-    public Result {
-      requireNonNull(type, "type is null");
-      requireNonNull(rawValue, "rawValue is null");
-    }
-
-    private Object checkRawValueType(Option.Type expectedType) {
-      if (type != expectedType) {
-        throw new IllegalStateException(type + " is not a " + expectedType);
-      }
-      return rawValue;
-    }
-
-    public boolean flag() {
-      return (boolean) checkRawValueType(FLAG);
-    }
-
-    @SuppressWarnings("unchecked")
-    public Optional<String> single() {
-      return (Optional<String>) checkRawValueType(SINGLE);
-    }
-
-    public String required() {
-      return (String) checkRawValueType(REQUIRED);
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<String> repeatable() {
-      return (List<String>) checkRawValueType(REPEATABLE);
-    }
-
-    public String[] varargs() {
-      return (String[]) checkRawValueType(VARARGS);
-    }
-  }
-
-  public interface Configuration<K> {
-    Configuration<K> with(K key, Option option);
-  }
-
-  static <K> Schema<Map<K, Result>> schemaMap(Consumer<? super Configuration<K>> consumer)  {
-    requireNonNull(consumer);
-    var map = new LinkedHashMap<K, Option>();
-    consumer.accept(new Configuration<>() {
-      @Override
-      public Configuration<K> with(K key, Option option) {
-        var result = map.putIfAbsent(key, option);
-        if (result != null) {
-          throw new IllegalStateException("duplicate key " + key);
-        }
-        return this;
-      }
-    });
-    return schemaMap(map);
-  }
-
-  static <T> Schema<Map<T, Result>> schemaMap(Map<? extends T, Option> optionMap)  {
-    requireNonNull(optionMap, "optionMap is null");
-    if (!optionMap.entrySet().spliterator().hasCharacteristics(Spliterator.ORDERED)) {
-      throw new IllegalArgumentException("the optionMap is not ordered");
-    }
-    var keys = optionMap.keySet().stream().toList();
-    var options = optionMap.values().stream().toList();
-    return new Schema<>(options, data ->
-        unmodifiableMap(range(0, options.size())
-            .boxed()
-            .collect(toMap(keys::get, i -> new Result(options.get(i).type(), data.get(i)), (_1, _2) -> { throw null;}, LinkedHashMap::new))));
-  }
-
   public static final class ArgumentBag<K> {
-    private record Argument(Option option, Object rawValue) { }
-
-    private final LinkedHashMap<K, Argument> argumentMap;
-
-    private ArgumentBag(LinkedHashMap<K, Argument> argumentMap) {
-      this.argumentMap = argumentMap;
-    }
-
-    private Object rawValue(K key, Option.Type expectedType) {
-      requireNonNull(key, "key is null");
-      var argument = argumentMap.get(key);
-      if (argument == null) {
-        throw new IllegalStateException("unknown key " + key);
-      }
-      if (argument.option.type() != expectedType) {
-        throw new IllegalStateException(argument.option.type() + " is not a " + expectedType);
-      }
-      return argument.rawValue;
-    }
-
-    public boolean flag(K key) {
-      return (boolean) rawValue(key, FLAG);
-    }
-
-    @SuppressWarnings("unchecked")
-    public Optional<String> single(K key) {
-      return (Optional<String>) rawValue(key, SINGLE);
-    }
-
-    public String required(K key) {
-      return (String) rawValue(key, REQUIRED);
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<String> repeatable(K key) {
-      return (List<String>) rawValue(key, REPEATABLE);
-    }
-
-    public String[] varargs(K key) {
-      return (String[]) rawValue(key, VARARGS);
-    }
-
-    @Override
-    public String toString() {
-      return argumentMap.toString();
-    }
-  }
-
-  static <K> Schema<ArgumentBag<K>> schemaBag(Consumer<? super Configuration<K>> consumer)  {
-    requireNonNull(consumer);
-    var map = new LinkedHashMap<K, Option>();
-    consumer.accept(new Configuration<>() {
-      @Override
-      public Configuration<K> with(K key, Option option) {
-        var result = map.putIfAbsent(key, option);
-        if (result != null) {
-          throw new IllegalStateException("duplicate key " + key);
-        }
-        return this;
-      }
-    });
-    return schemaBag(map);
-  }
-
-  static <K> Schema<ArgumentBag<K>> schemaBag(Map<? extends K, Option> optionMap) {
-    requireNonNull(optionMap, "optionMap is null");
-    if (!optionMap.entrySet().spliterator().hasCharacteristics(Spliterator.ORDERED)) {
-      throw new IllegalArgumentException("the optionMap is not ordered");
-    }
-    var keys = optionMap.keySet().stream().toList();
-    var options = optionMap.values().stream().toList();
-    return new Schema<>(options, data ->
-        new ArgumentBag<>(range(0, options.size())
-            .boxed()
-            .collect(toMap(keys::get, i -> new ArgumentBag.Argument(options.get(i), data.get(i)), (_1, _2) -> { throw null;}, LinkedHashMap::new))));
-  }
-
-
-  public static final class ValueBag {
     private final List<Option> options;
     private final List<Object> data;
-    private HashMap<String, Integer> indexMap;  // lazy
+    private final Function<List<Option>, Map<K, Integer>> indexMapFactory;
+    private Map<K, Integer> indexMap;  // lazy
 
-    private ValueBag(List<Option> options, List<Object> data) {
+    private ArgumentBag(List<Option> options, List<Object> data, Function<List<Option>, Map<K, Integer>> indexMapFactory) {
       this.options = options;
       this.data = data;
+      this.indexMapFactory = indexMapFactory;
     }
 
     private Object rawValue(int index, Option.Type expectedType) {
@@ -222,16 +75,14 @@ class EnumTests {
       return (String[]) rawValue(index, VARARGS);
     }
 
-    private HashMap<String, Integer> indexMap() {
+    private Map<K, Integer> indexMap() {
       if (indexMap != null) {
         return indexMap;
       }
-      return indexMap = range(0, options.size())
-          .boxed()
-          .collect(toMap(i -> options.get(i).names().iterator().next(), i -> i, (_1, _2) -> { throw null; }, LinkedHashMap::new));
+      return indexMap = indexMapFactory.apply(options);
     }
 
-    private Object rawValue(String name, Option.Type expectedType) {
+    private Object rawValue(K name, Option.Type expectedType) {
       var index = indexMap().get(name);
       if (index == null) {
         throw new IllegalStateException(name + " is not a valid name");
@@ -239,25 +90,25 @@ class EnumTests {
       return rawValue(index, expectedType);
     }
 
-    public boolean flag(String name) {
+    public boolean flag(K name) {
       return (boolean) rawValue(name, FLAG);
     }
 
     @SuppressWarnings("unchecked")
-    public Optional<String> single(String name) {
+    public Optional<String> single(K name) {
       return (Optional<String>) rawValue(name, SINGLE);
     }
 
-    public String required(String name) {
+    public String required(K name) {
       return (String) rawValue(name, REQUIRED);
     }
 
     @SuppressWarnings("unchecked")
-    public List<String> repeatable(String name) {
+    public List<String> repeatable(K name) {
       return (List<String>) rawValue(name, REPEATABLE);
     }
 
-    public String[] varargs(String name) {
+    public String[] varargs(K name) {
       return (String[]) rawValue(name, VARARGS);
     }
 
@@ -269,68 +120,58 @@ class EnumTests {
     }
   }
 
-  static Schema<ValueBag> schemaValue(Option... options) {
+  static Schema<ArgumentBag<String>> schemaArgument(Option... options) {
     requireNonNull(options, "options is null");
     var opt = List.of(options);
-    return new Schema<>(opt, data -> new ValueBag(opt, data));
+    return new Schema<>(opt, data -> new ArgumentBag<>(opt, data,
+        optionList -> range(0, optionList.size()).boxed().collect(toMap(i -> optionList.get(i).names().iterator().next(), i -> i))));
   }
+
+  public interface Configuration<K> {
+    Configuration<K> with(K key, Option option);
+  }
+
+  static <K> Schema<ArgumentBag<K>> schemaKeyed(Consumer<? super Configuration<K>> consumer)  {
+    requireNonNull(consumer);
+    var keys = new ArrayList<K>();
+    var options = new ArrayList<Option>();
+    consumer.accept(new Configuration<>() {
+      @Override
+      public Configuration<K> with(K key, Option option) {
+        requireNonNull(key, "key is null");
+        requireNonNull(option, "option is null");
+        keys.add(key);
+        options.add(option);
+        return this;
+      }
+    });
+    return new Schema<>(options, data -> new ArgumentBag<>(options, data,
+        optionList -> range(0, optionList.size()).boxed().collect(toMap(keys::get, i -> i))));
+  }
+
 
 
   // ---
 
-  @Test
-  void enumMap() {
-    enum Value { F, TEXT, R }
-
-    var optionMap = new LinkedHashMap<Value, Option>() {{
-        put(Value.F, FLAG.option("-f", "--flag"));
-        put(Value.TEXT, SINGLE.option("-t", "--text"));
-        put(Value.R, REQUIRED.option("-r"));
-    }};
-
-    var schema = schemaMap(optionMap);
-    var resultMap = Splitter.of(schema).split("-f", "value");
-
-    assertTrue(resultMap.get(Value.F).flag());
-    assertTrue(resultMap.get(Value.TEXT).single().isEmpty());
-    assertEquals("value", resultMap.get(Value.R).required());
-}
-
-
 
   @Test
-  void enumConf() {
-    enum Value { F, TEXT, R }
+  void stringArguments() {
+    var schema = EnumTests.schemaKeyed(conf -> conf
+        .with("optionFlag", FLAG.option("-f", "--flag"))
+        .with("optionText", SINGLE.option("-t", "--text"))
+        .with("optionR", REQUIRED.option("-r")));
+    var argumentBag = Splitter.of(schema).split("-f", "value");
 
-    var schema = schemaMap(conf -> conf
-        .with(Value.F, FLAG.option("-f", "--flag"))
-        .with(Value.TEXT, SINGLE.option("-t", "--text"))
-        .with(Value.R, REQUIRED.option("-r")));
-    var resultMap = Splitter.of(schema).split("-f", "value");
-
-    assertTrue(resultMap.get(Value.F).flag());
-    assertTrue(resultMap.get(Value.TEXT).single().isEmpty());
-    assertEquals("value", resultMap.get(Value.R).required());
+    assertTrue(argumentBag.flag("optionFlag"));
+    assertTrue(argumentBag.single("optionText").isEmpty());
+    assertEquals("value", argumentBag.required("optionR"));
   }
 
   @Test
-  void stringConf() {
-    var schema = EnumTests.schemaMap(conf -> conf
-        .with("flag", FLAG.option("-f", "--flag"))
-        .with("text", SINGLE.option("-t", "--text"))
-        .with("r", REQUIRED.option("-r")));
-    var resultMap = Splitter.of(schema).split("-f", "value");
-
-    assertTrue(resultMap.get("flag").flag());
-    assertTrue(resultMap.get("text").single().isEmpty());
-    assertEquals("value", resultMap.get("r").required());
-  }
-
-  @Test
-  void enumConfBag() {
+  void enumArguments() {
     enum Value { F, TEXT, R }
 
-    var schema = EnumTests.schemaBag(conf -> conf
+    var schema = schemaKeyed(conf -> conf
         .with(Value.F, FLAG.option("-f", "--flag"))
         .with(Value.TEXT, SINGLE.option("-t", "--text"))
         .with(Value.R, REQUIRED.option("-r")));
@@ -342,32 +183,19 @@ class EnumTests {
   }
 
   @Test
-  void stringConfBag() {
-    var schema = EnumTests.schemaBag(conf -> conf
-        .with("flag", FLAG.option("-f", "--flag"))
-        .with("text", SINGLE.option("-t", "--text"))
-        .with("r", REQUIRED.option("-r")));
-    var argumentBag = Splitter.of(schema).split("-f", "value");
-
-    assertTrue(argumentBag.flag("flag"));
-    assertTrue(argumentBag.single("text").isEmpty());
-    assertEquals("value", argumentBag.required("r"));
-  }
-
-  @Test
-  void stringValue() {
-    var schema = EnumTests.schemaValue(
+  void optionsArgument() {
+    var schema = EnumTests.schemaArgument(
         FLAG.option("-f", "--flag"),
         SINGLE.option("-t", "--text"),
         REQUIRED.option("-r"));
-    var valueBag = Splitter.of(schema).split("-f", "value");
+    var argumentBag = Splitter.of(schema).split("-f", "value");
 
-    assertTrue(valueBag.flag(0));
-    assertTrue(valueBag.single(1).isEmpty());
-    assertEquals("value", valueBag.required(2));
+    assertTrue(argumentBag.flag(0));
+    assertTrue(argumentBag.single(1).isEmpty());
+    assertEquals("value", argumentBag.required(2));
 
-    assertTrue(valueBag.flag("-f"));
-    assertTrue(valueBag.single("-t").isEmpty());
-    assertEquals("value", valueBag.required("-r"));
+    assertTrue(argumentBag.flag("-f"));
+    assertTrue(argumentBag.single("-t").isEmpty());
+    assertEquals("value", argumentBag.required("-r"));
   }
 }
