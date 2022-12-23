ca5d489d8c036d8949d4c09bfdd9a5f08e82bb95
forax <forax@univ-mlv.fr>
2022-12-23 13:57:13 +0100

    add javadoc

--- main/main/Help.java
+++ main/main/Help.java
@@ -5,6 +5,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Allow to specify {@link Option#help()} the help text} of an option defined as a record component.
+ * <p>
+ * This annotation allows to specify the help text as a string
+ * <pre>
+ *  record Command (
+ *    &#064;Help("this is an hep text")
+ *    boolean verbose
+ *  }
+ * </pre>
+ *
+ * or using several strings that will be joined using a space.
+ * <pre>
+ *  record Command (
+ *    &#064;Help({ "this is an hep text", "on several parts"})
+ *    boolean verbose
+ *  }
+ * </pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.RECORD_COMPONENT)
 public @interface Help {
--- main/main/Name.java
+++ main/main/Name.java
@@ -5,6 +5,33 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Allow to specify {@link Option#names() the names} of an option defined as a record component.
+ * <p>
+ * By default, the name of the option is derived from the name of the record component,
+ * with the '_' replaced by '-'.
+ * <pre>
+ *  record Command (
+ *    boolean __verbose
+ *  }
+ *  </pre>
+ *
+ * This annotation allows to specify a name thos is not a vid Java identifier
+ * <pre>
+ *  record Command (
+ *    &#064;Name("-v")
+ *    boolean verbose
+ *  }
+ * </pre>
+ *
+ * or several names
+ * <pre>
+ *  record Command (
+ *    &#064;Name({ "-v", "--verbose" })
+ *    boolean verbose
+ *  }
+ * </pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.RECORD_COMPONENT)
 public @interface Name {
