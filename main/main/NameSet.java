9448a6d2a207a9a29b0c3a805c563e018f00d88c
forax <forax@univ-mlv.fr>
2022-12-23 14:35:52 +0100

    simplify implementation (avoid code duplication)

--- main/main/NameSet.java
+++ main/main/NameSet.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.AbstractSet;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
@@ -51,20 +52,7 @@ final class NameSet extends AbstractSet<String> {
 
   public static Set<String> of(String... names) {
     requireNonNull(names, "names is null");
-    if (names.length == 0) {
-      throw new IllegalArgumentException("names is empty");
-    }
-    var set = new LinkedHashSet<String>();
-    for(var name: names) {
-      requireNonNull(name, "name is null");
-      if (name.isEmpty()) {
-        throw new IllegalArgumentException("one name is empty");
-      }
-      if (!set.add(name)) {
-        throw new IllegalArgumentException("duplicate names " + name);
-      }
-    }
-    return new NameSet(set);
+    return copyOf(Arrays.asList(names));
   }
 
   public static Set<String> copyOf(Collection<? extends String> names) {
