267f9bd58a0f256c27ed06e6ab1bcc44c21c3a55
forax <forax@univ-mlv.fr>
2022-12-26 14:43:22 +0100

    Add examples of arguments for each optional option

--- main/main/Option.java
+++ main/main/Option.java
@@ -64,15 +64,15 @@ public sealed interface Option<T> permits AbstractOption {
    */
   enum Type {
     BRANCH(null),
-    /** An optional flag, like {@code --verbose}. */
+    /** An optional flag, like {@code --verbose}, {@code quiet=true} or {@code quiet=false}. */
     FLAG(false),
-    /** An optional key-value pair, like {@code --version 47.11}. */
+    /** An optional key-value pair, like {@code --version 47.11} or {@code version=47.11}. */
     SINGLE(Optional.empty()),
-    /** An optional and repeatable key, like {@code --with alpha --with omega} */
+    /** An optional and repeatable key, like {@code --with alpha --with omega} or {@code --with=alpha,omega} */
     REPEATABLE(List.of()),
     /** A required positional option */
     REQUIRED(null),
-    /** A collection of all unhandled arguments. */
+    /** An array of all unhandled arguments. */
     VARARGS(new String[0]);
 
     final Object defaultValue;
