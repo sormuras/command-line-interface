80cd3feadfba9c7bbae0fe33a3f0fad59011f691
forax <forax@univ-mlv.fr>
2023-01-23 17:06:47 +0100

    remove unused imports

--- main/main/AbstractOption.java
+++ main/main/AbstractOption.java
@@ -4,7 +4,6 @@ import java.util.ArrayDeque;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
 
 import static java.util.Objects.requireNonNull;
 
--- main/main/RecordSchemaSupport.java
+++ main/main/RecordSchemaSupport.java
@@ -8,7 +8,6 @@ import java.lang.reflect.RecordComponent;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 /**
