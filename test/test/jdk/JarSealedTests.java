816168782235043d9ce7a3fcb3d65e8e02c818f4
Christian Stein <sormuras@gmail.com>
2025-05-22 12:16:38 +0200

    Clean up

--- test/test/jdk/JarSealedTests.java
+++ test/test/jdk/JarSealedTests.java
@@ -4,9 +4,10 @@ import static test.api.Assertions.assertEquals;
 
 import java.nio.file.Path;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Stream;
 import test.api.JTest;
 import test.api.JTest.Test;
 
@@ -18,7 +19,17 @@ public class JarSealedTests {
   }
 
   sealed interface Option {
-    record CreateArchive() implements Option {}
+    enum Mode {
+      CREATE,
+      UPDATE,
+      LIST;
+
+      Operation toOption() {
+        return new Operation(this);
+      }
+    }
+
+    record Operation(Mode mode) implements Option {}
 
     record File(Path jarFile) implements Option {}
 
@@ -34,7 +45,9 @@ public class JarSealedTests {
 
     static Option parse(String token, Scanner scanner) {
       return switch (token) {
-        case "-c", "--create" -> new CreateArchive();
+        case "-c", "--create" -> new Operation(Mode.CREATE);
+        case "-l", "--list" -> new Operation(Mode.LIST);
+        case "-u", "--update" -> new Operation(Mode.UPDATE);
         case "--verbose", "-v" -> new Verbose();
         case "--help", "-h", "-?", "/?" -> new Help();
         case "--version" -> new ShowVersionAndExit();
@@ -47,52 +60,73 @@ public class JarSealedTests {
   }
 
   record Options(List<Option> arguments) {
-    static Options of(String line) {
-      if (line.isBlank()) return new Options(List.of());
-      var options = new ArrayList<Option>();
+    Options() {
+      this(List.of());
+    }
+
+    Options add(Option option) {
+      if (option instanceof Option.Operation) {
+        if (operation().isPresent()) throw new IllegalStateException("Mode already present");
+      }
+      return new Options(Stream.concat(arguments.stream(), Stream.of(option)).toList());
+    }
+
+    Options parse(String line) {
+      if (line.isBlank()) return this;
       var delimiter = "\u0000";
       var source = String.join(delimiter, line.split("\\s+"));
       var scanner = new Scanner(source).useDelimiter(delimiter);
+      var options = this;
       while (scanner.hasNext()) {
         var token = scanner.next();
         var parsed = Option.parse(token, scanner);
-        options.add(parsed);
+        options = options.add(parsed);
       }
-      return new Options(List.copyOf(options));
+      return options;
+    }
+
+    Optional<Option.Operation> operation() {
+      return arguments.stream()
+          .filter(Option.Operation.class::isInstance)
+          .map(Option.Operation.class::cast)
+          .findAny();
     }
   }
 
   @Test
   void example1() {
-    var options = Options.of("--create --file classes.jar Foo.class Bar.class");
+    var actual = new Options().parse("--create --file classes.jar Foo.class Bar.class");
     var expected =
-        List.of(
-            new Option.CreateArchive(),
-            new Option.File(Path.of("classes.jar")),
-            new Option.Arg(Path.of("Foo.class")),
-            new Option.Arg(Path.of("Bar.class")));
-    assertEquals(expected, options.arguments());
+        new Options(
+            List.of(
+                Option.Mode.CREATE.toOption(),
+                new Option.File(Path.of("classes.jar")),
+                new Option.Arg(Path.of("Foo.class")),
+                new Option.Arg(Path.of("Bar.class"))
+            )
+        );
+    assertEquals(expected, actual);
   }
 
   @Test
   void example2() {
-    var options =
-        Options.of(
-            """
-            --create
-            --date 2021-01-06T14:36:00+02:00
-            --file classes.jar
-            Foo.class
-            Bar.class
-            """);
-
+    var actual =
+        new Options()
+            .parse(
+                """
+                --update
+                --date 2021-01-06T14:36:00+02:00
+                --file classes.jar
+                Foo.class
+                Bar.class
+                """);
     var expected =
-        List.of(
-            new Option.CreateArchive(),
-            new Option.Date(ZonedDateTime.parse("2021-01-06T14:36:00+02:00")),
-            new Option.File(Path.of("classes.jar")),
-            new Option.Arg(Path.of("Foo.class")),
-            new Option.Arg(Path.of("Bar.class")));
-    assertEquals(expected, options.arguments());
+        new Options()
+            .add(new Option.Operation(Option.Mode.UPDATE))
+            .add(new Option.Date(ZonedDateTime.parse("2021-01-06T14:36:00+02:00")))
+            .add(new Option.File(Path.of("classes.jar")))
+            .add(new Option.Arg(Path.of("Foo.class")))
+            .add(new Option.Arg(Path.of("Bar.class")));
+    assertEquals(expected, actual);
   }
 }
